65a70e7284f693058e69eb6adbacc8ad8dd155ff
hamidoucheasmae <76485278+hamidoucheasmae@users.noreply.github.com>
2021-10-25 10:44:08 +0100

    Partie-front-end terminer

--- systeme-authentification/src/App.js
+++ systeme-authentification/src/App.js
@@ -3,22 +3,57 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className="login-wrap">
+  
+  <div className="login-html">
+    <input id="tab-1" type="radio" name="tab" className="sign-in" defaultChecked />
+    <label htmlFor="tab-1" className="tab">CONNEXION</label>
+    <input id="tab-2" type="radio" name="tab" className="sign-up" />
+    <label htmlFor="tab-2" className="tab">INSCRIPTION</label>
+
+
+
+    
+    <div className="login-form">
+      <form className="sign-in-htm" action="./api/Users/login.php" method="POST">
+        <div className="group">
+          <input id="username" name="username" type="text" className="input" placeholder="Nom d'utilisateur"/>
+        </div>
+        <div className="group">
+          <input id="password" name="password" type="password" className="input" data-type="password" placeholder="Mot de passe" />
+        </div>
+        <div className="group">
+         <input type="submit" name="submit" className="button" defaultValue="Connexion" />
+        </div>
+        <div className="hr" />
+        
+      </form>
+      <form className="sign-up-htm" action="./api/Users/signup.php" method="POST">
+        <div className="group">
+          <input id="username" name="username" type="text" className="input" placeholder="Nom d'utilisateur"/>
+        </div>
+        <div className="group">
+          <input id="email" name="email" type="text" className="input" placeholder="Email" />
+        </div>
+        <div className="group">
+          <input id="password" name="password" type="password" className="input" data-type="password" placeholder="Mot de passe" />
+        </div>
+        <div className="group">
+          <input id="pass" type="password" className="input" data-type="password" placeholder="Confirmer mot de passe" />
+        </div>
+     
+        <div className="group">
+      
+        <input type="submit" name="submit" className="button" defaultValue="Créer votre compte" />
+      
+        </div>
+       
+
+       
+      </form>
     </div>
+  </div>
+</div>
   );
 }
 
