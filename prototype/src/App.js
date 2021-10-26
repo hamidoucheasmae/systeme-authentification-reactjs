76da68539b8cebed5149a796576933dddbd16166
hamidoucheasmae <76485278+hamidoucheasmae@users.noreply.github.com>
2021-10-26 09:57:01 +0100

    update navlink

--- prototype/src/App.js
+++ prototype/src/App.js
@@ -1,23 +1,30 @@
 
-// import './App.css';
+import './index.css';
 import React from "react";
-import {browserRouter, Switch ,Route, NavLink  }from "react-router-dom";
+import {BrowserRouter, Switch ,Route, NavLink  }from "react-router-dom";
 
+import Home from "./Home";
+import login from "./Login";
+import Dashboard from "./Dashboard";
 
 
 function App() {
   return (
     <div className="App">
-      <browserRouter>
-       <div className="hedear">
-      <NavLink>Home</NavLink>
-      <NavLink></NavLink>
-      <NavLink></NavLink>
+      <BrowserRouter>
+       <div className="header">
+      <NavLink exact activeClassName="active" to="/">Home</NavLink>
+      <NavLink activeClassName="active" to="/login">Login <small>Access without token only </small></NavLink>
+      <NavLink activeClassName="active" to="/dashboard" >Dashboard <small>Access without token only </small></NavLink>
       </div>  
       <div className="content">
-      
+    <Switch>
+       <Route exact path="/" component={Home}/>
+       <Route exact path="/login" component={login}/> 
+       <Route exact path="/dashboard" component={Dashboard}/>  
+    </Switch>  
       </div> 
-      </browserRouter>
+      </BrowserRouter>
     </div>
   );
 }
