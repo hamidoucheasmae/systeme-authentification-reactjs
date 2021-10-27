ce248957d582f3020f68f3d4bdf37078afde02b2
hamidoucheasmae <hamidouche.asmae@gmail.com>
2021-10-27 20:02:54 +0100

    update prototype

--- prototype/src/App.js
+++ prototype/src/App.js
@@ -1,15 +1,43 @@
 import './index.css';
-import React from "react";
+import React, { Component } from "react";
 
+class App extends Component{
+  state={
+    Text : ""
+  }
+  handleAdd = async e =>{
+await this.setState({
+  text : e.target.value
+})
+  
+  }
 
+ handleSubmit = e => {
+console.log(this.state.text);
+ }
+render()
 
-function App() {
-  return (
-    <div className="App">
-      <input type="text"/>
-      <button/>
+{
+return (
+
+  <div className="App-header">
+      <input 
+      onChange={this.handleAdd}
+       className="form-control" 
+      //  value={this.state.text}
+        type="text" id="text" placeholder="enter some text"/>
+      <br />
+      <button 
+       onClick={this.handleSubmit}
+      className="btn btn-success" id="submit">save </button>
     </div>
-  );
+
+);
+
 }
+
+}
+
+
 
 export default App;
